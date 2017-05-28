f5bb50a3c66bad512b572cce5ba469aea9f676ca
fara-moeeni <fara-moeeni@users.noreply.github.com>
2017-05-28 12:59:54 +0430

    Create extract.sh

---
+++ extract.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+for dir in `ls -1 | grep -viE ".sh" | xargs`
+  do
+    cd $dir
+    count=`ls | grep -iE ".rar" -c`
+    if [ "$count" == 0 ]; then
+      echo "there are no rar files in `basename $dir`. Trying next folder..."
+      cd ..
+      continue
+    else
+       type1=`ls | grep -iE "part1.rar" -c`
+	  if [ "$type1" == 0 ]; then
+            echo "rar single file found in $dir"
+            ls -1 | xargs unrar x
+            ls -1 *.rar | xargs rm -fv
+           cd ..
+          else
+            echo "splited rar files are found in $dir"
+            ls -1 *.part1* | xargs unrar x
+            find . -iname "*.part*.rar" -exec rm -rfv {} \;
+           cd ..
+          fi
+    fi
+  done
