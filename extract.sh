7039a97999f1d1422a2dcbe6947347fa475f2794
fara-moeeni <fara-moeeni@users.noreply.github.com>
2017-05-30 16:07:57 +0430

    Update extract.sh

--- extract.sh
+++ extract.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 #export IFS=$'\n'
+RED='\033[0;31m'
+GREEN='\033[0;33m'
 for dir in `ls -1 | grep -viE "extract.sh" | xargs -d '\n'`
   do
     cd $dir
     count=`ls | grep -iE ".rar" -c`
     if [ "$count" == 0 ]; then
-      echo ">>> There are no rar files in `basename $dir`. Trying next folder... >>>"
+      echo -e "$RED >>> There are no rar files in `basename $dir`. Trying next folder... >>>"
       cd ..
       continue
     else
-       type1=`ls | grep -iE "part.?1.rar" -c`
+       type1=`ls | grep -iE "part1.rar|part01.rar" -c`
 	  if [ "$type1" == 0 ]; then
-            echo ">>> Single .rar file found in $dir <<<"
+            echo -e "$RED >>> Single .rar file found in $dir <<<"
             ls -1 | xargs unrar x
             ls -1 *.rar | xargs rm -fv
            cd ..
           else
             echo ">>> Splitted rar files are found in $dir <<<"
-            ls -1 *.part1* | xargs unrar x
+            type2=`ls | grep -iE "part1.rar" -c`
+              if [ "$type2" == 0 ]; then
+                echo -e "$GREEN Part0X mode found"
+                ls -1 *.part01* | xargs unrar x
+              else
+                echo -e "$GREEN PartX mode found"
+                ls -1 *.part1* | xargs unrar x
+              fi
             find . -iname "*.part*.rar" -exec rm -rfv {} \;
            cd ..
           fi
