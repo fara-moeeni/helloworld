6d0ba242b9918170e69b7d797abb8d66a5cc200c
fara-moeeni <fara-moeeni@users.noreply.github.com>
2017-06-03 09:47:44 +0430

    Update extract.sh

--- extract.sh
+++ extract.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 #export IFS=$'\n'
 RED='\033[0;31m'
-GREEN='\033[0;33m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
 for dir in `ls -1 | grep -viE "extract.sh" | xargs -d '\n'`
   do
     cd $dir
     count=`ls | grep -iE ".rar" -c`
     if [ "$count" == 0 ]; then
       echo -e "$RED >>> There are no rar files in `basename $dir`. Trying next folder... >>>"
+      echo -e "$NC"
       cd ..
       continue
     else
        type1=`ls | grep -iE "part1.rar|part01.rar" -c`
 	  if [ "$type1" == 0 ]; then
             echo -e "$RED >>> Single .rar file found in $dir <<<"
+            echo -e "$NC"
             ls -1 | xargs unrar x
             ls -1 *.rar | xargs rm -fv
            cd ..
@@ -21,10 +24,12 @@ for dir in `ls -1 | grep -viE "extract.sh" | xargs -d '\n'`
             echo ">>> Splitted rar files are found in $dir <<<"
             type2=`ls | grep -iE "part1.rar" -c`
               if [ "$type2" == 0 ]; then
-                echo -e "$GREEN Part0X mode found"
+                echo -e "$YELLOW Part0X mode found"
+                echo -e "$NC"
                 ls -1 *.part01* | xargs unrar x
               else
-                echo -e "$GREEN PartX mode found"
+                echo -e "$YELLOW PartX mode found"
+                echo -e "$NC"
                 ls -1 *.part1* | xargs unrar x
               fi
             find . -iname "*.part*.rar" -exec rm -rfv {} \;
